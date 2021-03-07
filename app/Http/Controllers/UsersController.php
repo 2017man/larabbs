6d26e9cebd5219f52bb41ced9e8718d1c756b41e
满向向 <3398284534@qq.com>
2021-03-07 13:56:57 +0800

    文件上传功能封装

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,9 +20,14 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $upload, User $user)
     {
-        $user->update($request->all());
+        $data   = $request->all();
+        $result = $upload->save($request->file('avatar'), 'avatars', $user->id);
+        if ($result) {
+            $data['avatar'] = $result['path'];
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
