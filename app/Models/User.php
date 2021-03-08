a2ab2410f164d49b43e3f3e0ddae1bedd946182d
满向向 <3398284534@qq.com>
2021-03-08 23:44:12 +0800

    用户可以编辑删除自己的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,14 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    /**
+     * 授权通用处理
+     * @param $model
+     * @return bool
+     */
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
