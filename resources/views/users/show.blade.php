30c67afaac5f305e6011d326dd86c20579502ede
满向向 <3398284534@qq.com>
2021-03-08 19:28:12 +0800

    用户发布的话题

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -27,7 +27,9 @@
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
             <div class="card ">
                 <div class="card-body">
-                    <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+                    <h1 class="mb-0" style="font-size:22px;">{{ $user->name }}
+                        <small>{{ $user->email }}</small>
+                    </h1>
                 </div>
             </div>
             <hr>
@@ -35,7 +37,13 @@
             {{-- 用户发布的内容 --}}
             <div class="card ">
                 <div class="card-body">
-                    暂无数据 ~_~
+                    <div class="card-body">
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+                        </ul>
+                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    </div>
                 </div>
             </div>
 
