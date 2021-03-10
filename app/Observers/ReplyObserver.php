8fd9a53a6261ca3dcb81a0d5d3b851f7e045a9d3
满向向 <3398284534@qq.com>
2021-03-10 23:29:58 +0800

    回复列表和发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,22 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+    /**
+     * 话题回复数+1
+     */
+    public function created(Reply $reply)
     {
-        //
+//        $reply->topic->increment('reply_count', 1);
+        //最佳实践
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
-    public function updating(Reply $reply)
+    /**
+     * 防XSS漏洞攻击
+     */
+    public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
